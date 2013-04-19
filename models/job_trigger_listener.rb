9dab57883dd21f5e14c055d98d2891e32b81c6aa
Dr Nic Williams <drnicwilliams@gmail.com>
2013-04-18 22:03:53 -0700

    Strip back JobTriggerListener to be ready for only #updated events

--- models/job_trigger_listener.rb
+++ models/job_trigger_listener.rb
@@ -23,49 +23,20 @@ class JobTriggerListener
     @logger ||= Logger.getLogger(self.class.name)
   end
   
-  # Called after a new job is created and added to@link jenkins.model.Jenkinsend,
-  # before the initial configuration page is provided.
-  # 
-  # This is useful for changing the default initial configuration of newly created jobs.
-  # For example, you can enable/add builders, etc.
-  def created(item)
-  end
-
-  # Called after a new job is created by copying from an existing job.
-  #
-  # For backward compatibility, the default implementation of this method calls@link #onCreated(Item)end.
-  # If you choose to handle this method, think about whether you want to call super.onCopied or not.
-  #
-  # @param [Jenkins::Model::Item] The source item that the new one was copied from. Never null.
-  # @param [Jenkins::Model::Item] The newly created item. Never null.
-  def copied(src_item, item)
-  end
-
-  # Called after all the jobs are loaded from disk into@link jenkins.model.Jenkinsend
+  # Called after all the jobs are loaded from disk into jenkins.model.Jenkins
   # object.
   def loaded()
-  end
-
-  # Called right before a job is going to be deleted.
-  #
-  # At this point the data files of the job is already gone.
-  # @param [Jenkins::Model::Item] The item being deleted.
-  def deleted(item)
-  end
-
-  # Called after a job is renamed.
-  #
-  # @param [Jenkins::Model::Item] The item being renamed
-  # @param The old name of the job.
-  # @param The new name of the job. Same as@link Item#getName()end.
-  def renamed(item, oldName, newName)
+    logger.info("items loaded")
   end
 
   # Called after a job has its configuration updated.
   #
   # @param [Jenkins::Model::Item] The item being updated
   def updated(item)
-    logger.info("update(#{item.name})")
+    logger.info("update - #{item.native.name} (#{item.native.inspect})")
+    logger.info item.native.getPublishersList().inspect
+    logger.info item.native.getBuildWrappers().inspect
+    logger.info item.native.getBuildWrappers().class
   end
 
   # Called at the begenning of the orderly shutdown sequence to
